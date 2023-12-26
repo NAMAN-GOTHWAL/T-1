26f516933421a130136fbc645e68b6ef1058cbcd
Naman Gothwal <144902978+NAMAN-GOTHWAL@users.noreply.github.com>
2023-12-26 22:50:13 +0530

    Add files via upload

---
+++ Test-1/index.js
@@ -0,0 +1,76 @@
+// Ans 1
+// Types of variables-3(let,const,var)
+// let and const are block scoped whereas var is global scoped.
+// can be redeclared-var
+// can be reassigned-let
+// ...... ...... ... ... 
+// Ans 2.
+// The == operator only compares the value but === operator compares value and data type as well. \
+// ... ... ... .. .. ..
+// Ans 3
+// In html body 
+// {/* <button id="btn" onclick="hide()">press</button> */}
+// IN js 
+// function hide(){
+
+        //   document.getElementById("btn").style.display = "none"
+    
+    //  }
+// ... ... .. .. ..
+// Ans 4
+// let  arFn = (x,y)=>{
+
+
+    //     let c = a+b
+    //     console.log(c)
+    
+    
+    //  }
+    
+    //  arrowFun(7,5)
+// ... ... .. .. ..
+// Ans 5
+// let array = [12,18,45,23,98,76,56,90,84,68]
+
+
+//   let nArray =  array.filter((value)=>{
+//     return value>50
+//   })
+// console.log(nArray)
+// ... ... .. .. ..
+// Ans 6
+// function Fn(){
+    
+    //         let x = "ha ha ha iam a function"
+    //         console.log(x)
+    
+    //     }
+    
+    //     setTimeout(Fn,5000)
+    // ... ... .. .. ..
+    // Ans 7
+    // function Fname(){
+        //     let x= "you can repeat anytime"
+        //     console.log(x)
+
+    //  setInterval(fInt,2000)
+    // ... ... .. .. ..
+    // Ans 8
+    // function showPassword(){
+        //     let password = document.getElementById("pss")            
+        //  if(password.type=="password"){ 
+            //     password.type = "text"           
+            //  }
+            //  else{
+                //      password.type = "password"
+                //  }
+                //            }
+                
+                // ... ... .. .. ..
+
+            
+            
+        
+
+    
+        
